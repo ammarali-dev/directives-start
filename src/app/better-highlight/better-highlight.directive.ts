3c2438dd3d55cb3846a97652d52b87f406866d27
ammarali-dev <tabraiz.ammarali@gmail.com>
2024-03-05 13:55:21 +0500

    Directive Deep Dive Completed

---
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -0,0 +1,44 @@
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  Input,
+  Renderer2,
+} from '@angular/core';
+
+@Directive({
+  selector: '[appBetterHighlight]',
+})
+export class BetterHighlightDirective {
+  @Input() defaultColor: string = 'transparent';
+  @Input('appBetterHighlight') highlightColor: string = 'blue';
+  @HostBinding('style.backgroundColor')
+  backgroundColor: string;
+
+  constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
+  ngOnInit() {
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'background-color',
+    //   'blue'
+    // );
+    this.backgroundColor = this.defaultColor;
+  }
+  @HostListener('mouseenter') mouseOver(eRTRventData: Event) {
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'background-color',
+    //   'blue'
+    // );
+    this.backgroundColor = this.highlightColor;
+  }
+  @HostListener('mouseleave') mouseLeave(eRTRventData: Event) {
+    // this.renderer.setStyle(
+    //   this.elementRef.nativeElement,
+    //   'background-color',
+    //   'transparent'
+    //   );
+    this.backgroundColor = this.defaultColor;
+  }
+}
